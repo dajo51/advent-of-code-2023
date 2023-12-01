e34d13b63937f68487ad75a05302a12bdac7d39c
David Josiger <david.josiger@ida.me>
2023-12-01 11:07:16 +0100

    fix: refactor part a

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,12 +1,14 @@
 def a(input: str) -> int:
-    with open(input, "r") as f:
-        lines = f.readlines()
     total = 0
-    for line in lines:
-        line = ''.join(i for i in line.strip() if i.isdigit())
-        line = line[0] + line[-1]
-        if line:
-            total += int(line)
+    with open(input, "r") as f:
+        for line in f:
+            line = ''.join(i for i in line.strip() if i.isdigit())
+            line = line[0] + line[-1]
+            if line:
+                total += int(line)
     return total
+
+def b(input: str) -> int:
+    pass
 
 print(a("day_1/input.txt"))
