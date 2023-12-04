e5d623e859df44563d2c4eece1cf40078222e486
David Josiger <david.josiger@ida.me>
2023-12-04 09:23:57 +0100

    day 2 part b

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -29,4 +29,34 @@ def a(input):
     return sol
 
 
+def b(input):
+    sol_dict = {"red": 12, "green": 13, "blue": 14}
+    sol = 0
+
+    with open(input, "r") as f:
+        for line in f:
+            game_id = int("".join([s for s in line.split(":")[0] if s.isdigit()]))
+            sets = line.strip().split(";")
+            sets[0] = "".join(sets[0].split(":")[1:])
+            green_min = 0
+            red_min = 0
+            blue_min = 0
+
+            for set in sets:
+                cubes = set.strip().split(",")
+                cubes = [[int(item.strip().split(" ")[0]), item.strip().split(" ")[1]] for item in cubes]
+                for pair in cubes:
+                    for key, val in sol_dict.items():
+                        if key in pair:
+                            if key == "red":
+                                red_min = max(red_min, pair[0])
+                            if key == "green":
+                                green_min = max(green_min, pair[0])
+                            if key == "blue":
+                                blue_min = max(blue_min, pair[0])
+            sol += red_min * green_min * blue_min
+        
+    return sol
+
 print(a("day_2/input.txt"))
+print(b("day_2/input.txt"))
