09218c9446581fd5fee90276830d170cebf11966
David Josiger <david.josiger@ida.me>
2023-12-09 15:01:19 +0100

    day 4 final

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -1,18 +1,30 @@
 def a(input):
-    with open(input) as f:
-        sol = 0
-        for line in f:
-            points = 1
-            numbers = line.strip().split(": ")[1]
-            win = set([i for i in numbers.split("|")[0].split(" ") if i.isdigit()])
-            game = set([i for i in numbers.split("|")[1].split(" ") if i.isdigit()])
-            wins = len(win.intersection(game))
-            if wins:
-                for i in range(wins - 1):
-                    points = points * 2
-                sol += points
+    lines = open(input, "r").readlines()
+    sol = 0
+    for line in lines:
+        points = 1
+        numbers = line.strip().split(": ")[1]
+        win = set([i for i in numbers.split("|")[0].split(" ") if i.isdigit()])
+        game = set([i for i in numbers.split("|")[1].split(" ") if i.isdigit()])
+        wins = len(win.intersection(game))
+        if wins:
+            for i in range(wins - 1):
+                points = points * 2
+            sol += points
 
     return sol
 
 
-print(a("day_4/input.txt"))
+def b(input):
+    lines = open(input, "r").readlines()
+    cards = [1] * len(lines)
+    for i, line in enumerate(lines):
+        x, y = map(str.split, line.split("|"))
+        n = len(set(x) & set(y))
+        for j in range(i + 1, min(i + 1 + n, len(lines))):
+            cards[j] += cards[i]
+
+    return sum(cards)
+
+
+print(f"sol a: {a('day_4/input.txt')} sol b: {b('day_4/input.txt')}")
